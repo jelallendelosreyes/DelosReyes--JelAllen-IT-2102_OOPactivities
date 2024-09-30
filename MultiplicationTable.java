d944ef0c8a63f4cff9923265cdb31b5c70f7a97d
jelallendelosreyes <23-06632@g.batstate-u.edu.ph>
2024-09-30 09:42:28 +0800

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class a here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+      
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the multiplication table: ");
+        int size = scanner.nextInt();
+        
+
+        int[][] table = new int[size][size];
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        
+        System.out.println("\nMultiplication Table:\n");
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%4d", table[i][j]);
+            }
+            System.out.println(); 
+        }
+        
+        scanner.close();
+    }
+}
---
+++ getArrayMean.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class h here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class getArrayMean {
+
+
+    public static double getArrayMean(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return (double) sum / array.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number: ");
+            numbers[i] = scanner.nextInt();
+        }
+
+     
+        double mean = getArrayMean(numbers);
+
+       
+        System.out.printf("Mean of array is: %.2f%n", mean);
+        
+        
+        scanner.close();
+    }
+}
